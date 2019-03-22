d69d33851a53a716d212dc7e2ac2ef26bca167d8
Mihael Haluga <mihael.haluga@thespian.hr>
2019-03-22 12:14:02 +0100

    Hard coded showcase

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar is-spaced has-shadow" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <div class="navbar-item">
+          <h2>Friday Showcase
+            <b-icon
+              style="margin-left: 10px; vertical-align: middle;"
+              icon="sunglasses"
+              size="is-medium">
+            </b-icon>
+          </h2>
+        </div>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item button is-light">
+            Add project
+          </a>
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <section>
+              <b-table
+                :data="showcases"
+                :columns="columns"
+                detailed
+              >
+                <template #detail="{ row }">
+                  <Showcase :project="row"></Showcase>
+                </template>
+              </b-table>
+            </section>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Showcase from "./components/Showcase.vue";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components: { Showcase },
+  data() {
+    return {
+      showcases: [
+        {
+          name: 'Course hunted!',
+          description: "Webpage for scraping 'darkweb' tutorials site to watch videos add-free.",
+          goal: 'Learn to use Typescript with Vue',
+          link: 'https://course-hunted.firebaseapp.com/',
+          date: '8 March 2019'
+        },
+        {
+          name: 'Vue dynamic templates',
+          description: 'See goal',
+          goal: 'Server side async vue components without special webpack setup',
+          link: '',
+          date: '1 March 2019'
+        }
+      ],
+      columns: [
+        { field: 'name', label: 'Project'},
+        { field: 'description', label: 'About' },
+        { field: 'goal', label: 'Goal' },
+        { field: 'date', label: 'Date' },
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Showcase.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="showcase">
+    <iframe
+      frameborder="0" width="100%"
+      :src="project.link"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  mounted() {
+  // eslint-disable-next-line
+    console.log('mounted')
+  },
+  destroyed() {
+    console.log('destroy');
+  }
+}
+</script>
+
+<style scoped>
+  .showcase {
+    min-height: 400px;
+  }
+  .showcase iframe {
+    height: 100vh;
+  }
+</style>
